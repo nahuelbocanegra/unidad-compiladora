2d35f6ce480d90952781af4763127bdc372c37d7
isma <ismagustin15@gmail.com>
2025-06-07 04:01:57 -0300

    Actualiza menú principal en main.py

--- Programacion/main.py
+++ Programacion/main.py
@@ -9,6 +9,46 @@ from crud_dispositivos import (
     consultar_estado
 )
 
+
+# def dispo():
+    
+#     while True:
+
+#         print("\n1 - Listar dispositivo ")
+#         print("2 - Buscar dispositivo ")
+#         print("3 - Agregar dispositivo ")
+#         print("4 - Eliminar dispositivo ")
+#         print("5 - Controlar aire")
+#         print("6 - Salir \n")
+
+
+
+#         try:
+#             opcion = int(input("Elija una opcion:   "))
+            
+
+#             if opcion == 1:
+#                 listar_dispositivos()
+#             elif opcion == 2:
+#                 nombre_dispositivo=input("Ingrese el nombre de dispositivo que desea buscar: ")
+#                 print(buscar_dispositivos(nombre_dispositivo))
+#             elif opcion == 3:
+#                 nuevo_dispositivo=input("Ingrese el nombre de dispositivo que desea agregar: ")
+#                 agregar_dispositivos(nuevo_dispositivo)
+#             elif opcion == 4:
+#                 dispositivo=input("Ingrese el nombre de dispositivo que desea eliminar: ")
+#                 eliminar_dispositivos(dispositivo)
+#             elif opcion == 5:
+#                 controlar_aire()
+#             elif opcion == 6:
+#                 print("vuelva pronto! ")
+#                 break
+#             else:
+#                 print("por favor ingrese un numero del 1 al 5")
+
+#         except:
+#             print("Entrada no valida,por favor ingrese un numero del 1 al 5")
+
 def mostrar_menu():
     while True:
         print("\n=== Menú de Dispositivos Inteligentes ===")
@@ -45,7 +85,7 @@ def mostrar_menu():
 
         elif opcion == "6":
             nombre = input(" Ingrese el nombre del dispositivo: ")
-            clave = input(" Ingrese el atributo a modificar (ej: 'ubicacion', 'tipo'): ")
+            clave = input(" Desea cambiar el modo (ej: 'auto', 'manual'): ")
             valor = input(" Ingrese el nuevo valor: ")
             modificar_dispositivo(nombre, **{clave: valor})
 
@@ -59,3 +99,5 @@ def mostrar_menu():
 
         else:
             print(" Opción inválida. Por favor intente de nuevo.")
+
+mostrar_menu()
